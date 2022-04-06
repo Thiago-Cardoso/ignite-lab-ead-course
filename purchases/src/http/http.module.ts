2b669e50709e386de8039a08751b4417d284464f
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2022-04-06 20:49:47 -0300

    feat: configuration database with prisma

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from 'src/database/database.module';
 import { TestController } from 'src/http/test.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DatabaseModule],
   controllers: [TestController],
 })
 export class HttpModule {}
--- purchases/src/http/test.controller.ts
+++ purchases/src/http/test.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 
 @Controller('test')
 export class TestController {
+  constructor(private prisma: PrismaService) {}
+
   @Get('ok')
   @UseGuards(AuthorizationGuard)
   hello() {
-    return 'Ok';
+    return this.prisma.customer.findMany();
   }
 }
