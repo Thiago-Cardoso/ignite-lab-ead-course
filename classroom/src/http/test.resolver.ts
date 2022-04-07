4f200a96656e97138a187ad0023c99d9163b9518
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2022-04-06 21:55:38 -0300

    feat: configuration graphQL in classroom and purchases

--- classroom/src/http/test.resolver.ts
+++ classroom/src/http/test.resolver.ts
@@ -1,14 +1,15 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query} from '@nestjs/graphql';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
 
-@Controller('test')
-export class TestController {
+@Resolver()
+export class TestResolver {
   constructor(private prisma: PrismaService) {}
 
-  @Get('ok')
+  @Query(() => String)
   @UseGuards(AuthorizationGuard)
   hello() {
-    return this.prisma.customer.findMany();
+    return 'Hello world';
   }
 }
